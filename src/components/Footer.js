13d5470ace0c55ee3f6bd5e49ea7381f12e8aaed
Nelson Chinedu <egwuchinedu69@gmail.com>
2019-01-14 12:19:10 -0800

    installed react-router-dom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,57 +18,57 @@ const Footer = ()=>{
 
             <div className="footer-second">
               <div className="footer-top">
-                <a href="/">Politics</a>
+                <p>Politics</p>
               </div>
               <div className="footer-bottom">
-                <p>Business</p>
-                <p>Markets</p>
-                <p>Tech</p>
-                <p>Luxury</p>
+                <p><a href="/">Business</a></p>
+                <p><a href="/">Markets</a></p>
+                <p><a href="/">Tech</a></p>
+                <p><a href="/">Luxury</a></p>
               </div>
             </div>
 
             <div className="footer-third">
               <div className="footer-top">
-                <a href="/">Featured</a>
+                <p>Featured</p>
               </div>
               <div className="footer-bottom">
-                <p>Football</p>
-                <p>Golf</p>
-                <p>Tennis</p>
-                <p>Motorsport</p>
-                <p>Horseracing</p>
-                <p>Sailing</p>
+                <p><a href="/">Football</a></p>
+                <p><a href="/">Golf</a></p>
+                <p><a href="/">Tennis</a></p>
+                <p><a href="/">Motorsport</a></p>
+                <p><a href="/">Horseracing</a></p>
+                <p><a href="/">Sailing</a></p>
               </div>
             </div>
 
             <div className="footer-fourth">
               <div className="footer-top">
-                <a href="/">FAQ</a>
+                <p>FAQ</p>
               </div>
               <div className="footer-bottom">
-                <p>Aviation</p>
-                <p>Business</p>
-                <p>Traveller</p>
-                <p>Destinations</p>
-                <p>Features</p>
-                <p>Food/Drink</p>
-                <p>Hotels</p>
-                <p>Partner Hotels</p>
+                <p><a href="/">Aviation</a></p>
+                <p><a href="/">Business</a></p>
+                <p><a href="/">Traveller</a></p>
+                <p><a href="/">Destinations</a></p>
+                <p><a href="/">Features</a></p>
+                <p><a href="/">Food/Drink</a></p>
+                <p><a href="/">Hotels</a></p>
+                <p><a href="/">Partner Hotels</a></p>
               </div>
             </div>
 
             <div className="footer-fifth">
               <div className="footer-top">
-                <a href="/">&plus;More</a>
+                <p>&plus;More</p>
               </div>
               <div className="footer-bottom">
-                <p>Fashion</p>
-                <p>Design</p>
-                <p>Architechture</p>
-                <p>Arts</p>
-                <p>Autos</p>
-                <p>Luxury</p>
+                <p><a href="/">Fashion</a></p>
+                <p><a href="/">Design</a></p>
+                <p><a href="/">Architechture</a></p>
+                <p><a href="/">Arts</a></p>
+                <p><a href="/">Autos</a></p>
+                <p><a href="/">Luxury</a></p>
               </div>
             </div>
 
