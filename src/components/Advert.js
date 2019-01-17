0637edaa1dda8d20df70f14a7034592d94aee63f
Nelson Chinedu <egwuchinedu69@gmail.com>
2019-01-16 22:10:36 -0800

    fix some changes on header

--- src/components/Advert.js
+++ src/components/Advert.js
@@ -7,11 +7,11 @@ const Advert = ()=>{
             <div className="advert-wrapper">
                 <div className="upper-advert">
                     <p className="category-top">Breaking News</p>
-                    <p className="news-headline">some text</p>
+                    <p className="news-headline">Mark Zuckerberg: Everything Is Fine, Actually</p>
                 </div>
                 <div className="bottom-advert">
                     <p className="category-bottom">International</p>
-                    <p className="news-headline">some text</p>
+                    <p className="news-headline">Democrats head into 2019 split on everything but Trump</p>
                 </div>
             </div>
             <div className=" right-advert">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import logo from './image/logo.png';
 
 const Header = ()=>{
     return(
         <div id="main-header">
             <div className="header-wrapper">
             <div className="header-lt">
-                <a href="/" className="brand-title">The News Paper</a>
-                <p className="brand-motto">Everything News &amp; Lifestyle...</p>
+                {/* <a href="/" className="brand-title">The News Paper</a> */}
+                {/* <p className="brand-motto">Everything News &amp; Lifestyle...</p> */}
+                <img src={logo} />
             </div>
-            <div className="header-rt">
-                <input type="text" placeholder="Search" className="input-search" />
-            </div>
+            {/* <div className="header-rt"> */}
+                {/* <input type="text" placeholder="Search" className="input-search" /> */}
+            {/* </div> */}
             </div>
         </div>
     )
